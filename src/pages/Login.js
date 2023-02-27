159f2542cb155e8fba76e7cedc889cf64980bd40
Marcelo Bernardo <bernardomarcelo33@gmail.com>
2023-02-27 10:50:15 -0300

    Requisito 01

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,79 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getEmail } from '../redux/actions/index';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    isDisabled: true,
+  };
+
+  handleClick = () => {
+    const { history, dispatch } = this.props;
+    const { email } = this.state;
+    history.push('/carteira');
+    return dispatch(getEmail(email));
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    }, this.validate);
+  };
+
+  validate = () => {
+    const { email, password } = this.state;
+    const minCharacter = 6;
+    const passwordValidate = password.length >= minCharacter;
+    const emailValidate = (/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/).test(email);
+    this.setState({ isDisabled: !(passwordValidate && emailValidate) });
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password, isDisabled } = this.state;
+    return (
+      <div>
+        <from>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={ email }
+            onChange={ this.handleChange }
+            data-testid="email-input"
+          />
+          <input
+            id="password"
+            name="password"
+            type="password"
+            value={ password }
+            onChange={ this.handleChange }
+            data-testid="password-input"
+          />
+          <button
+            type="button"
+            disabled={ isDisabled }
+            onClick={ this.handleClick }
+          >
+            Entrar
+          </button>
+        </from>
+      </div>
+    );
   }
 }
 
-export default Login;
+// const mapStateToProps = (state) => ({
+//   userEmail: state.user.email,
+// });
+
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default connect()(Login);
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,19 @@
+import { GET_EMAIL } from '../actions/index';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case GET_EMAIL:
+    return {
+      email: action.payload,
+    };
+  default:
+    return state;
+  }
+};
+
+export default user;
 // Esse reducer será responsável por tratar as informações da pessoa usuária
