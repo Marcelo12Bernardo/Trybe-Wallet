4884699cc27529692b3ae90d11ab53d01c329025
Marcelo Bernardo <bernardomarcelo33@gmail.com>
2023-06-02 11:55:15 -0300

    teste

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 class Header extends Component {
   render() {
     const { email, expenses } = this.props;
+    // teste
     return (
       <header>
         <section>
