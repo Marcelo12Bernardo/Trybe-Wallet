0d7230cffcc751960a3cbd6d03f79a398314ed75
Marcelo Bernardo <bernardomarcelo33@gmail.com>
2023-06-07 12:10:55 -0300

    fix: altera rotas

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -22,7 +22,7 @@ class Login extends React.Component {
     const { email,password  } = this.state;
     dispatch(getEmail({ email, password }));
     dispatch(fetchCurrency());
-    return history.push('/carteira');
+    return history.push('/Trybe-Wallet/carteira');
   };
 
   validForm = () => {
